310a07ba8556aae7f9a580965c54ce097989ba32
WeiLunLo <b02703078>
2017-01-10 23:20:16 +0800

    data

--- file/global.R
+++ file/global.R
@@ -6,5 +6,5 @@ library(jiebaR)
 library(jiebaRD)
 mixSeg <- worker()
 cutter <- worker(bylines = T)
-teepr2 <- "https://weilunlo.github.io/ContentFarm/file/teepr.csv"
+teepr2 <- "https://weilunlo.github.io/KTVcaculator/testKTVcalculator/teepr.csv"
 teepr <- read.csv(teepr2, fileEncoding = "BIG5")

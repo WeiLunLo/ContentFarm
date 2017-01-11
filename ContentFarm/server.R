3e4cac4a82b8711153121d4373fa80dd094d29db
WeiLunLo <b02703078>
2017-01-11 18:03:12 +0800

    jjj

--- ContentFarm/server.R
+++ ContentFarm/server.R
@@ -1,37 +1,52 @@
 library(shiny)
 source("global.R")
 server <- function(input, output,session) {
-  
+  #part
   output$part1 = renderUI({
     titlepart1 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/teepr_",input$part,".png")
     tags$img(src = titlepart1) 
   })
-  
   output$part2 = renderUI({
     titlepart2 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/Bomb01_",input$part,".png")
     tags$img(src = titlepart2) 
   })
-  
   output$part3 = renderUI({
     titlepart3 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/ptt01_",input$part,".png")
     tags$img(src = titlepart3) 
   })
-  
   output$part4 = renderUI({
     titlepart4 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/EBCbuzz_",input$part,".png")
     tags$img(src = titlepart4) 
   })
+  #hl
+  output$hl1 = renderUI({
+    titlehl1 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/teepr_",input$react,"_",input$part,".png")
+    tags$img(src = titlehl1) 
+  })
+  output$hl2 = renderUI({
+    titlehl2 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/Bomb01_",input$react,"_",input$part,".png")
+    tags$img(src = titlehl2) 
+  })
+  output$hl3 = renderUI({
+    titlehl3 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/ptt01_",input$react,"_",input$part,".png")
+    tags$img(src = titlehl3) 
+  })
+  output$hl4 = renderUI({
+    titlehl4 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/EBCbuzz_",input$react,"_",input$part,".png")
+    tags$img(src = titlehl4) 
+  })
   
+  #title
   output$title1 = renderUI({
     titlepath1 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/scatter/dist_",input$fanpage,".png")
     tags$img(src = titlepath1) 
   })
-  
   output$title2 = renderUI({
     titlepath2 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/rg/lm_",input$fanpage,".png")
     tags$img(src = titlepath2) 
   })
   
+  #time
   output$time = renderUI({
     timepath = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/timeavg/time_",input$fanpage,".png")
     tags$img(src = timepath) 
--- ContentFarm/ui.R
+++ ContentFarm/ui.R
@@ -10,13 +10,13 @@ shinyUI(
                         h3("「內容農場（Content Farm）是指以取得網路流量為主要目標，圖謀網路廣告等商業利益的專業公司」", style = "color:red"),
                         h4("被歸為內容農場的網站藉由渲染力強的標題吸引點閱率，其標題往往有以下特徵:"),
 
-                        p("- 聳動—語不驚人死不休 「這件事讓十億人都驚呆了!」"),
+                        p("- 聳動—語不驚人死不休", strong("「這件事讓十億人都驚呆了!」")),
                           
-                        p("- 吊胃口—話只說一半 「在知道這件事後，她的反應令人驚訝!」"),
+                        p("- 吊胃口—話只說一半", strong("「在知道這件事後，她的反應令人驚訝!」")),
                           
-                        p("- 羅列項目—總是要列出好幾點 「十個你不能錯過的絕美風景!」"),
+                        p("- 羅列項目—總是要列出好幾點", strong("「十個你不能錯過的絕美風景!」")),
                           
-                        p("- 語氣誇張—無論什麼主題都要搭配驚嘆語氣 「………!!!!!!」"),
+                        p("- 語氣誇張—無論什麼主題都要搭配驚嘆語氣", strong("「………!!!!!!」")),
                         br(),
                         h4("最近臉書上內容農場猖獗，版面上充斥著各種標題聳動的文章，然而點進去後內容往往令人大失所望。 不甘一再受騙，我們決定藉由這次報告探討內容農場的奧秘，想要討論的問題如下:"),
                         p("- 內容農場所下的標題有什麼常見的性質"),
@@ -29,27 +29,38 @@ shinyUI(
                         sidebarPanel(
                           selectInput("fanpage", label = h3("以來源區分"), 
                                       choices = list("Teepr趣味新聞" = "teepr","Bomb01" = "Bomb01",
-                                                     "Ptt01" = "ptt01", "躁咖" = "EBCbuzz"), 
-                                      selected = 0),
+                                                     "Ptt01" = "ptt01", "躁咖(EBCbuzz)" = "EBCbuzz"), 
+                                      selected = "teepr"),
                           selectInput("part", label = h3("以詞性區分"), 
                                       choices = list("名詞" = "n","動詞" = "v",
                                                      "形容詞" = "a", "副詞" = "d"), 
-                                      selected = 1),
+                                      selected = "n"),
                           selectInput("react", label = h3("以迴響數區分"), 
-                                      choices = list("全部" = 0,"高迴響數" = 1,
-                                                     "低迴響數" = 2), 
-                                      selected = 0)
+                                      choices = list("高迴響數" = "h",
+                                                     "低迴響數" = "l"), 
+                                      selected = "h")
                         ),
                         mainPanel(
                           tabsetPanel(
                             tabPanel("濫用標點符號",
+                                     h3("驚嘆號「!」是每個農場下標時的必備元素，刪節號「…」則為躁咖之外的三個農場所用。"),
+p("「瑞典的資源回收『台灣必須現在立馬跟進』，神到不可置信的效率連垃圾都已經缺貨了！」"),
+p("「妻子在臨睡前暗示著丈夫『她想要離婚』，沒想到丈夫意料之外的回答讓她哭了一整夜…」"),
+p("「這個學生『故意讓小偷拿走手機』，接著他就展開了追蹤整個後續的記錄片…！」"),
                                      img(src = "https://weilunlo.github.io/ContentFarm/ContentFarm/image/symbol/symbol.png")),
                             tabPanel("各詞性的高頻詞",
                                      htmlOutput('part1'),
                                      htmlOutput('part2'),
                                      htmlOutput('part3'),
-                                     htmlOutput('part4')),
-                            tabPanel("高低迴響用詞分析"),
+                                     htmlOutput('part4'),
+                                     h3("「大家」、「網友」、「人」為幾個常見用字，有製造輿論的效果。"),
+                                     p("「 大家一開始看到這團棉花時還想說是誰弄掉了，結果看到棉花球「一抬起頭」時才發現是地表最萌驚喜！」"),
+                                     p("「她問退伍的爺爺「中校12級退役大嗎？」， 網友都跪了…」")),
+                            tabPanel("高低迴響用詞分析",
+                                     htmlOutput('hl1'),
+                                     htmlOutput('hl2'),
+                                     htmlOutput('hl3'),
+                                     htmlOutput('hl4')),
                             tabPanel("時間分析",
                                      htmlOutput('time')),
                             tabPanel("標題長度",
@@ -62,11 +73,17 @@ shinyUI(
              tabPanel("模型",
                       fluidPage(
                         titlePanel("模型"),
-                        sidebarPanel(),
+                        sidebarPanel(
+                              textInput("text", "Text:", "text here"),
+                              actionButton("go", "Go")
+                            
+                        ),
                         mainPanel(
                           tabsetPanel(
-                            tabPanel("SVM"),
-                            tabPanel("LM")
+                            tabPanel("SVM",
+                                     textOutput("prediction")),
+                            tabPanel("LM",
+                                     textOutput("prediction"))
                           )
                         )
                       )

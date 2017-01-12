e6594c28405e91f74cedcff5b7820a551d01fdef
WeiLunLo <b02703078>
2017-01-12 12:28:49 +0800

    slide

--- ContentFarm/server.R
+++ ContentFarm/server.R
@@ -1,9 +1,12 @@
 library(shiny)
 source("global.R")
-server <- function(input, output,session) {
+
+shinyServer(
+  function(input, output) {
+  
   #part
-  output$part1 = renderUI({
-    titlepart1 = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/teepr_",input$part,".png")
+  output$part1 <- renderUI({
+    titlepart1 = paste("https://weilunlo.github.io/ContentFarm/ContentFarm/image/",input$part,"/teepr_",input$part,".png",sep = "")
     tags$img(src = titlepart1) 
   })
   output$part2 = renderUI({
@@ -49,9 +52,40 @@ server <- function(input, output,session) {
   #time
   output$time = renderUI({
     timepath = paste0("https://weilunlo.github.io/ContentFarm/ContentFarm/image/timeavg/time_",input$fanpage,".png")
-    tags$img(src = timepath) 
+    tags$img(src = timepath)
   })
   
-  
-  
-}
+  output$partresult1 = renderUI({
+    if (input$fanpage == "teepr"){
+      tags$h2("Teepr")
+    }else if(input$fanpage == "Bomb01") {
+      tags$h2("Bomb01")
+    }else if(input$fanpage == "ptt01") {
+      tags$h2("Ptt01")
+    }else {
+      tags$h2("EBCbuzz")
+    }
+  })
+  output$partresult2 = renderUI({
+    if (input$part == "n"){
+      tags$h4("「大家」、「網友」、「人」為幾個常見用字，有製造輿論的效果。")
+    }else if(input$part == "v") {
+      tags$h4("「讓」榮登農場最愛用動詞。")
+    }else if(input$part == "a") {
+      tags$h4("「大」強調程度不一般，可以強化渲染力。 ")
+    }else {
+      tags$h4("大量使用「都」字，營造「大家一致認可」的感覺。")
+    }
+  })
+  output$partresult3 = renderUI({
+    if (input$part == "n"){
+      tags$h4("例：「 大家一開始看到這團棉花時還想說是誰弄掉了，結果看到棉花球「一抬起頭」時才發現是地表最萌驚喜！」")
+    }else if(input$part == "v") {
+      tags$h4("例：「他在17歲「變性成女生」後竟淪落到賣淫自殘，1年後決定「重返男兒身」的他 讓大家都看傻了！」")
+    }else if(input$part == "a") {
+      tags$h4("例：「艾迪瑞德曼「比電影還精彩」真實背景 大揭密！貴族家庭出身的他說「很對不起威廉王子…」」")
+    }else {
+      tags$h4("例：「她騎著哈雷機車跟男友單膝下跪求婚，說完會加碼送房產後「一轉頭」…大家 都發出哇一聲！」")
+    }
+  })
+})
--- ContentFarm/temp.R
+++ ContentFarm/temp.R
@@ -2,26 +2,26 @@ library(shiny)
 
 shinyUI(
   navbarPage(theme = shinytheme("united"),
-             "內容農場的秘密讓全台灣都驚呆了!",
+             "內容農場",
              tabPanel("緣起",
                       fluidPage(
-                        div(
-                        h1("專案緣起"),
-                        h3("「內容農場（Content Farm）是指以取得網路流量為主要目標，圖謀網路廣告等商業利益的專業公司」",style = "color:#930000"),
+                        titlePanel("專案緣起"),
+                        
+                        h3("「內容農場（Content Farm）是指以取得網路流量為主要目標，圖謀網路廣告等商業利益的專業公司」", style = "color:red"),
                         h4("被歸為內容農場的網站藉由渲染力強的標題吸引點閱率，其標題往往有以下特徵:"),
                         
-                        p("- 聳動 —語不驚人死不休", strong("「這件事讓十億人都驚呆了!」",style ="font-style: italic"),style = "color:#F75000"),
+                        p("- 聳動—語不驚人死不休", strong("「這件事讓十億人都驚呆了!」")),
                         
-                        p("- 吊胃口 —話只說一半", strong("「在知道這件事後，她的反應令人驚訝!」",style ="font-style: italic"),style = "color:#F75000"),
+                        p("- 吊胃口—話只說一半", strong("「在知道這件事後，她的反應令人驚訝!」")),
                         
-                        p("- 羅列項目 —總是要列出好幾點", strong("「十個你不能錯過的絕美風景!」",style ="font-style: italic"),style = "color:#F75000"),
+                        p("- 羅列項目—總是要列出好幾點", strong("「十個你不能錯過的絕美風景!」")),
                         
-                        p("- 語氣誇張 —無論什麼主題都要搭配驚嘆語氣", strong("「………!!!!!!」",style ="font-style: italic"),style = "color:#F75000"),
+                        p("- 語氣誇張—無論什麼主題都要搭配驚嘆語氣", strong("「………!!!!!!」")),
                         br(),
                         h4("最近臉書上內容農場猖獗，版面上充斥著各種標題聳動的文章，然而點進去後內容往往令人大失所望。 不甘一再受騙，我們決定藉由這次報告探討內容農場的奧秘，想要討論的問題如下:"),
-                        h4(strong("- 內容農場所下的標題有什麼常見的性質"),style = "color:#F75000"),
-                        h4(strong("- 具有何種特性的標題比較能夠被關注"),style = "color:#F75000") , style = "color:black ; border-radius:5px" )
-                        )
+                        p("- 內容農場所下的標題有什麼常見的性質"),
+                        p("- 具有何種特性的標題比較能夠被關注")
+                      )
              ),
              tabPanel("資料探索",
                       fluidPage(
@@ -44,24 +44,22 @@ shinyUI(
                           tabsetPanel(
                             tabPanel("濫用標點符號",
                                      img(src = "https://weilunlo.github.io/ContentFarm/ContentFarm/image/symbol/symbol.png"),
-                                     h2(strong("驚嘆號「!」是每個農場下標時的必備元素，刪節號「…」則為躁咖之外的三個農場所用。")),
-                                     h4("「瑞典的資源回收『台灣必須現在立馬跟進』，神到不可置信的效率連垃圾都已經缺貨了！」"),
-                                     h4("「妻子在臨睡前暗示著丈夫『她想要離婚』，沒想到丈夫意料之外的回答讓她哭了一整夜…」"),
-                                     h4("「這個學生『故意讓小偷拿走手機』，接著他就展開了追蹤整個後續的記錄片…！」")
-                                    ),
+                                     h3("驚嘆號「!」是每個農場下標時的必備元素，刪節號「…」則為躁咖之外的三個農場所用。"),
+                                     p("「瑞典的資源回收『台灣必須現在立馬跟進』，神到不可置信的效率連垃圾都已經缺貨了！」"),
+                                     p("「妻子在臨睡前暗示著丈夫『她想要離婚』，沒想到丈夫意料之外的回答讓她哭了一整夜…」"),
+                                     p("「這個學生『故意讓小偷拿走手機』，接著他就展開了追蹤整個後續的記錄片…！」")),
                             tabPanel("各詞性的高頻詞",
                                      
                                      htmlOutput('part1'),
                                      htmlOutput('part2'),
                                      htmlOutput('part3'),
                                      htmlOutput('part4'),
-                                     htmlOutput('partresult1'),
-                                     htmlOutput('partresult2'),
-                                     htmlOutput('partresult3')
                                      
-                           ),
+                                     h3("「大家」、「網友」、「人」為幾個常見用字，有製造輿論的效果。"),
+                                     p("「 大家一開始看到這團棉花時還想說是誰弄掉了，結果看到棉花球「一抬起頭」時才發現是地表最萌驚喜！」"),
+                                     p("「她問退伍的爺爺「中校12級退役大嗎？」， 網友都跪了…」")),
                             
-                          tabPanel("高低迴響用詞分析",
+                            tabPanel("高低迴響用詞分析",
                                      htmlOutput('hl1'),
                                      htmlOutput('hl2'),
                                      htmlOutput('hl3'),
@@ -75,7 +73,19 @@ shinyUI(
                         )
                       )
              ),
-             tabPanel("團隊分工")
-             
+             tabPanel("模型",
+                      fluidPage(
+                        titlePanel("模型"
+                        ),
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("SVM",
+                                     textOutput("prediction")),
+                            tabPanel("LM",
+                                     textOutput("prediction"))
+                          )
+                        )
+                      )
+             )
   )
 )
